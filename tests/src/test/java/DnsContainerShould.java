c7d56b8a2d6a47c9ad00115c31c645be9edebfc1
LH <l.holota@outlook.com>
2020-06-23 16:43:24 +0200

    test: Refactoring to use the testcontainers-extensions library (#31)

--- tests/src/test/java/DnsContainerShould.java
+++ tests/src/test/java/DnsContainerShould.java
@@ -1,16 +1,42 @@
+import helpers.DnsImageTagResolver;
+import io.homecentr.testcontainers.containers.GenericContainerEx;
+import io.homecentr.testcontainers.images.PullPolicyEx;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.xbill.DNS.*;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class DnsContainerShould extends ContainerTestBase {
-    private static final Logger logger = LoggerFactory.getLogger(ContainerTestBase.class);
+public class DnsContainerShould {
+    private static final Logger logger = LoggerFactory.getLogger(DnsContainerShould.class);
+
+    private static GenericContainerEx _container;
+
+    @BeforeClass
+    public static void setUp() {
+        _container = new GenericContainerEx<>(new DnsImageTagResolver())
+                .withRelativeFileSystemBind(Paths.get("..", "example"), "/config")
+                .withImagePullPolicy(PullPolicyEx.never())
+                .waitingFor(Wait.forHealthcheck());
+
+        _container.start();
+        _container.followOutput(new Slf4jLogConsumer(logger));
+    }
+
+    @AfterClass
+    public static void cleanUp() {
+        _container.close();
+    }
 
     @Test
     public void resolveExternalZoneViaForwarders() throws UnknownHostException, TextParseException {
@@ -43,7 +69,7 @@ public class DnsContainerShould extends ContainerTestBase {
 
     private SimpleResolver createResolver() throws UnknownHostException {
         SimpleResolver resolver = new SimpleResolver("127.0.0.1");
-        resolver.setPort(super.getMappedUdpPort(53));
+        resolver.setPort(_container.getMappedUdpPort(53));
 
         return resolver;
     }
